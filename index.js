c1ca49df214ff1dddc5e2065c5191beabb36aa24
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-08 13:46:31 -0800

    adding generate quote function

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function init() {
     document.getElementById("text").innerText = "Developer";
     document.getElementById("author").innerText = "The Author";
 };
+
+function generateQuote() {
+    console.log("generate quote");
+}
