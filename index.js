65d124c1e3bc960111977f3c0347e64a1e304375
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-08 13:53:31 -0800

    adding quotes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const quoteBank = [
+    “The best is yet to be,
+    Try to be a rainbow in someone's cloud,
+    Do good and good will come to you,
+    A positive mindset brings positive things,
+    Positivity always wins
+    When things go wrong, don't go with them.,
+    Live life to the fullest and focus on the positive,
+    Keep looking up"
+];
 window.onload = init;
 function init() {
     generateQuote();
